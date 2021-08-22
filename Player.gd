e5966874b24ba4081b9ef88160a5ddef9b5f1544
sirtau <contact@hamidmousa.com>
2021-08-22 10:02:04 +1000

    Added Animations
    Animations will play based on velocity and facingDir
    If either vel.x or vel.y are > 0, the respective Move animation will play.
    func play_animation will only start playing an animation if it's not already playing. Allows it to loop correctly.
    When idle, facingDir is used to determine which Idle frame to use.

--- Player.gd
+++ Player.gd
@@ -17,7 +17,7 @@ var vel : Vector2 = Vector2()
 var facingDir : Vector2 = Vector2()
 
 onready var rayCast = get_node("RayCast2D")
-
+onready var anim = get_node("AnimatedSprite")
 
 
 func _physics_process(delta):
@@ -39,3 +39,28 @@ func _physics_process(delta):
 	vel = vel.normalized()
 
 	move_and_slide(vel * moveSpeed)
+	manage_animations()
+
+func manage_animations ():
+	if vel.x > 0:
+		play_animation("MoveRight")
+	elif vel.x < 0:
+		play_animation("MoveLeft")
+	elif vel.y < 0:
+		play_animation("MoveUp")
+	elif vel.y > 0:
+		play_animation("MoveDown")	
+	elif facingDir.x == 1:
+		play_animation("IdleRight")
+	elif facingDir.x == -1:
+		play_animation("IdleLeft")
+	elif facingDir.y == -1:
+		play_animation("IdleUp")
+	elif facingDir.y == 1:
+		play_animation("IdleDown")
+	
+	
+		
+func play_animation (anim_name):
+	if anim.animation != anim_name:
+		anim.play(anim_name)
