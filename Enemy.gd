11e1ed71dce8d98479d030f3c990026de2a67df0
sirtau <contact@hamidmousa.com>
2021-08-22 11:42:24 +1000

    Setup Interaction, XP, level up and Attack systems. Code cleanup.
    When "interact" is pressed, try_interact function runs. Raycast draws based on direction and runs either take_amage or uses on_interact(self).
    passing (self) through lets the target of the interaction know what it's interacting with.

--- Enemy.gd
+++ Enemy.gd
@@ -14,9 +14,28 @@ var chaseDist : int = 400
 onready var timer = get_node("Timer")
 onready var target = get_node("/root/MainScene/Player")
 
+func _ready():
+	timer.wait_time = attackRate
+	timer.start()
+
 func _physics_process(delta):
 	var dist = position.distance_to(target.position)
 	
 	if dist > attackDist and dist < chaseDist:
 		var vel = (target.position - position).normalized()
 		move_and_slide(vel * moveSpeed)
+
+
+func _on_Timer_timeout():
+	if position.distance_to(target.position) <= attackDist:
+		target.take_damage(damage)
+
+func take_damage(dmgToTake):
+	curHp -= dmgToTake
+	
+	if curHp <= 0:
+		die()
+
+func die ():
+	target.give_xp(xpToGive)
+	queue_free()
--- Player.gd
+++ Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
-var curHP : int = 10
-var maxHP : int = 10
+var curHp : int = 10
+var maxHp : int = 10
 var moveSpeed : int = 250
-var damage : int = 1
+var damage : int = 3
 
 var gold : int = 0
 var curLevel : int = 0
@@ -11,7 +11,7 @@ var curXp : int = 0
 var xpToNextLevel : int = 50
 var xpToLevelIncreaseRate : float = 1.2
 
-var interactist : int = 70
+var interactDist : int = 70
 
 var vel : Vector2 = Vector2()
 var facingDir : Vector2 = Vector2()
@@ -35,13 +35,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		vel.x += 1
 		facingDir = Vector2(1, 0)
-
+	
 	vel = vel.normalized()
-
+	
 	move_and_slide(vel * moveSpeed)
 	manage_animations()
 
-func manage_animations ():
+func manage_animations():
 	if vel.x > 0:
 		play_animation("MoveRight")
 	elif vel.x < 0:
@@ -58,9 +58,44 @@ func manage_animations ():
 		play_animation("IdleUp")
 	elif facingDir.y == 1:
 		play_animation("IdleDown")
+
+func _process(delta):
+	if Input.is_action_just_pressed("interact"):
+		try_interact()
+
+func try_interact():
+	rayCast.cast_to = facingDir * interactDist
 	
-	
-		
-func play_animation (anim_name):
+	if rayCast.is_colliding():
+		if rayCast.get_collider() is KinematicBody2D:
+			rayCast.get_collider().take_damage(damage)
+		elif rayCast.get_collider().has_method("on_interact"):
+			rayCast.get_collider().on_interact(self)
+
+func play_animation(anim_name):
 	if anim.animation != anim_name:
 		anim.play(anim_name)
+
+func give_xp(amount):
+	curXp += amount
+	
+	if curXp >= xpToNextLevel:
+		level_up()
+
+func level_up():
+	var overflowXp = curXp - xpToNextLevel
+	xpToNextLevel *= xpToLevelIncreaseRate
+	curXp = overflowXp
+	curLevel += 1
+
+func take_damage(dmgToTake):
+	curHp -= dmgToTake
+	
+	if curHp <= 0:
+		die()
+
+func die():
+	get_tree().reload_current_scene()
+	
+
+
